853749d6a23a6dc51db1c4ef587c76849bec7983
afrikannerd <amolocaleb@gmail.com>
2018-06-15 14:41:16 +0300

    Initial Commit for School CMS

---
+++ core/View.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Core;
+
+/**
+ * Description of View
+ *
+ * @author afrikannerd <https://github.com/afrikannerd>
+ */
+class View {
+    //put your code here
+}
---
+++ core/auth/Authenticate.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Core\Auth;
+/**
+ *
+ * @author afrikannerd <https://github.com/afrikannerd>
+ */
+trait Authenticate {
+    //put your code here
+    
+}
